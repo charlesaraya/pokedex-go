142c50c7fdaa51f93a9469459fcfa0c9ae674282
charlesaraya <charlesaraya@gmail.com>
2025-04-24 19:41:51 +0200

    feat: add registry of commands and exit CLI support

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	"strings"
 )
 
+type Command struct {
+	Name        string
+	Description string
+	Command     func() error
+}
+
+var Registry = map[string]Command{
+	"exit": {
+		Name:        "exit",
+		Description: "Exit the Pokedex CLI",
+		Command:     commandExit,
+	},
+}
+
 func cleanInput(text string) []string {
 	if len(text) == 0 {
 		return []string{}
@@ -16,12 +30,30 @@ func cleanInput(text string) []string {
 	return fields
 }
 
+func commandExit() error {
+	defer os.Exit(0)
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
+		// get input from user
 		scanner.Scan()
 		userInput := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", userInput[0])
+
+		// Check if the user entered a command
+		for command, data := range Registry {
+			if command == userInput[0] {
+				if err := data.Command(); err != nil {
+					fmt.Printf("Error: %s command produced an error", command)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
+		}
 	}
+
 }
